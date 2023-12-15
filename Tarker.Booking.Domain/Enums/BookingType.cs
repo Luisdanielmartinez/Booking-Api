a1625ef49bc4416a2586911d7bb71516b7d050c4
Danielcurso <luisdanielcurso@gmail.com>
2023-12-14 21:40:59 -0400

    initial commit

---
+++ Tarker.Booking.Domain/Enums/BookingType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarker.Booking.Domain.Enums
+{
+    //this is the type of booking 
+    public enum BookingType
+    {
+        Documentation,
+        Transfer,
+        Renewal
+    }
+}
